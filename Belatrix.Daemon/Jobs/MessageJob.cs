ed88d43377a90fe18af8c12e7027867bfc546159
Julio Velarde <cevegaju@hotmail.com>
2019-07-30 01:08:32 -0500

    Add project files.

---
+++ Belatrix.Daemon/Jobs/MessageJob.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using Quartz;
+using System.Threading.Tasks;
+
+namespace Belatrix.Daemon.Jobs
+{
+    public class MessageJob : IJob
+    {
+        private readonly ILogger<MessageJob> _logger;
+        public MessageJob(ILogger<MessageJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Message processed.");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Belatrix.Daemon/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Belatrix.Daemon.Jobs;
+using Belatrix.Daemon.Schedules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
+
+namespace Belatrix.Daemon
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Add Quartz services
+            services.AddSingleton<IJobFactory, SingletonJobFactory>();
+            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+            services.AddHostedService<QuartzHostedService>();
+
+            // Add our job
+            services.AddSingleton<MessageJob>();
+            services.AddSingleton(new JobSchedule(
+                jobType: typeof(MessageJob),
+                cronExpression: "0/5 * * * * ?")); // run every 5 seconds
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
